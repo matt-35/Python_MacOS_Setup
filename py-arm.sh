14010f428f60c79d3477094d06be9aa753ca3d2e
Matt <matthew.j.beadle@gmail.com>
2023-05-03 09:40:05 -0500

    initial commit

---
+++ py-arm.sh
@@ -0,0 +1,16 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/matt/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/matt/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/Users/matt/miniforge3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/matt/miniforge3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
